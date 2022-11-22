29ad8e85f4d0ac5f448b6f079ef0132be83b9bbb
Wuesis <wuesis983@gmail.com>
2022-11-21 19:59:10 -0600

    Project -> FirstCommit (images added).

---
+++ src/GUI.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class GUI extends JInternalFrame {
+
+
+    public GUI(){
+        super("Hipodromo");
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(1200,900);
+        setResizable(false);
+    }
+
+}
